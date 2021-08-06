bc6669a35c19b451e228a80f976d6f1a2a337eca
Dmytro Buryak <dmytro.buryak@gmail.com>
2021-08-06 16:09:22 +0300

    SS-34: made Greeter public

--- non-functional-lib/src/main/java/shared/libs/nonFunctionalLib/Greetings.java
+++ non-functional-lib/src/main/java/shared/libs/nonFunctionalLib/Greetings.java
@@ -2,7 +2,7 @@ package shared.libs.nonFunctionalLib;
 
 import java.util.Random;
 
-class Greetings {
+public class Greetings {
     public static String helloThere() {
         var rndIndex = new Random().nextInt();
         return "General Kenobi? - " + rndIndex;
