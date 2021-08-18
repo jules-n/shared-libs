1de44cdf3d101e43d5c9dafbb8d663aff667b0ac
ynero <ynero@softserveinc.com>
2021-08-18 16:31:27 +0300

    SS-67: Added lib for common dtos

---
+++ common-dtos/src/main/java/dtos/DeviceDTO.java
@@ -0,0 +1,17 @@
+package dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class DeviceDTO {
+    private UUID id;
+    private Event event;
+}
