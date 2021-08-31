bb78c3fb7f5bd5bb6e7424e131af33f3f697b66d
ynero <ynero@softserveinc.com>
2021-08-31 11:58:22 +0300

    feature/SS-32: Added dto for pub-sub interaction between execution and device service for adding pipeline to devices

---
+++ common-dtos/src/main/java/dtos/PipelineDevicesDTO.java
@@ -0,0 +1,19 @@
+package dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PipelineDevicesDTO {
+    private UUID pipelineId;
+    private List<UUID> deviceIds;
+    private String name;
+}
