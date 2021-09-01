a22e3e593e8473d3295990505bce926235bfcac2
ynero <ynero@softserveinc.com>
2021-09-01 10:09:09 +0300

    Added dto for topic device-pipeline

--- common-dtos/src/main/java/dtos/PipelineDevicesDTO.java
+++ common-dtos/src/main/java/dtos/PipelineDevicesDTO.java
@@ -6,14 +6,13 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
-import java.util.UUID;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class PipelineDevicesDTO {
-    private UUID pipelineId;
-    private List<UUID> deviceIds;
+    private String pipelineId;
+    private List<String> deviceIds;
     private String portName;
 }
---
+++ common-dtos/src/main/java/dtos/PipelineIdDTO.java
@@ -0,0 +1,14 @@
+package dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PipelineIdDTO {
+    private UUID pipelineId;
+}
