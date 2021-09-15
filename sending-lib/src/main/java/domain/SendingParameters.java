e4114b2640d4f11d1a0caa6c5a6d4ba6e93fb94e
ynero <ynero@softserveinc.com>
2021-09-15 13:01:39 +0300

    feature/SS-80: Added serializable implementation

--- sending-lib/src/main/java/domain/SendingParameters.java
+++ sending-lib/src/main/java/domain/SendingParameters.java
@@ -4,12 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.util.Map;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class SendingParameters {
+public class SendingParameters implements Serializable {
     private String sendingClassName;
     private Map<String, Object> parameters;
 }
