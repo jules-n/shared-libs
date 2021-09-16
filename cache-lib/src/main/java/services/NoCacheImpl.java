f9303146292e0e2499bcd61845af0d9e88d6749e
ynero <ynero@softserveinc.com>
2021-09-16 17:30:17 +0300

    feature/SS-80: Added no-cache implementation

---
+++ cache-lib/src/main/java/services/NoCacheImpl.java
@@ -0,0 +1,44 @@
+package services;
+
+import models.Entry;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class NoCacheImpl<K, V> implements CacheService<K, V>{
+
+    @Override
+    public void save(K key, V value, Integer expirationTime, TimeUnit timeUnit) {
+
+    }
+
+    @Override
+    public void save(K key, V value) {
+
+    }
+
+    @Override
+    public V get(K key) {
+        return null;
+    }
+
+    @Override
+    public boolean delete(K key) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteMany(K... keys) {
+        return false;
+    }
+
+    @Override
+    public void saveMany(List<Entry<K, V>> entries, Integer expirationTime, TimeUnit timeUnit) {
+
+    }
+
+    @Override
+    public void saveMany(List<Entry<K, V>> entries) {
+
+    }
+}
