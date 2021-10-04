9589aa19c4662b538375ec2d12c6660d8e105c66
ynero <ynero@softserveinc.com>
2021-10-04 16:56:54 +0300

    feature/healthchecks: Removed imports

--- healthchecks/src/main/java/com/ynero/ss/health/autoconfigure/PubSubAutoConfiguration.java
+++ healthchecks/src/main/java/com/ynero/ss/health/autoconfigure/PubSubAutoConfiguration.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration(proxyBeanMethods = false)
+@Configuration
 @ConditionalOnClass(PubsubMessage.class)
 public class PubSubAutoConfiguration {
 
